119dfab06f6279e1c1326d0cd60f629ee1e4b477
NurullaWasHere <NurullaWasHere>
2023-04-29 11:47:35 +0600

    signdate

--- controllers/SignController.js
+++ controllers/SignController.js
@@ -53,7 +53,7 @@ const getSignsSortedByDate = async (req,res) => {
         const result = await sign.findAll({
             attributes: [
                 [fn('DATE', col('signDate')), 'signDate'], 
-                [literal('array_agg(json_build_object(\'signDate\', signDate, \'user_id\', user_id) ORDER BY sign_date ASC)'), 'user_ids']
+                [literal('array_agg(json_build_object(\'signDate\', signDate, \'user_id\', user_id) ORDER BY signDate ASC)'), 'user_ids']
             ],
             group: [fn('DATE', col('signDate'))]
         })
